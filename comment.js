26a6e6d39b2891a4f8ddff763fd37376e5bee349
Manojkumar <manojbhosale@users.noreply.github.com>
2023-10-23 07:47:56 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,24 +1,2 @@
-// create web server with express
-const express = require("express");
-const app = express();
-const path = require("path");
-const port = 3000;
-// import data
-const comments = require("./data/comments");
-const contacts = require("./data/contacts");
-const products = require("./data/products");
-// import routes
-const commentsRoutes = require("./routes/comments");
-const contactsRoutes = require("./routes/contacts");
-const productsRoutes = require("./routes/products");
-// use routes
-app.use("/comments", commentsRoutes);
-app.use("/contacts", contactsRoutes);
-app.use("/products", productsRoutes);
-// serve static files
-app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/css", express.static(path.join(__dirname, "css")));
-// create server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+// create web server with comment function
+// 2016-01-22
