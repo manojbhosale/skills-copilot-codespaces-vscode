6450db0f1c44dd6bae673907298ab0ea9f0e268a
Manojkumar <manojbhosale@users.noreply.github.com>
2023-10-23 07:27:29 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,92 +1,83 @@
-// create web server
+// create web server with node.js
+// 2016-03-23
 
-// create web server
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
-
-// load data
-const comments = require('./comments.json');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
 // set view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// use static files
-app.use(express.static(path.join(__dirname, 'public')));
+// set static directory
+app.use(express.static('public'));
 
-// use body parser
-app.use(express.urlencoded({ extended: false }));
+// set body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
-// use express session
-const session = require('express-session');
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
-}));
-
-// use connect flash
-const flash = require('connect-flash');
-app.use(flash());
-
-// use passport
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-
-// passport config
-const User = require('./models/user');
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// define routes
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home page' });
+// set router
+app.get('/', function(req, res){
+	res.render('index');
 });
 
-app.get('/comments', (req, res) => {
-  res.render('comments', { title: 'Comments page', comments: comments });
+app.get('/form', function(req, res){
+	res.render('form');
 });
 
-app.get('/comments/new', (req, res) => {
-  res.render('new', { title: 'New comment' });
+app.post('/form_receiver', function(req, res){
+	var title = req.body.title;
+	var description = req.body.description;
+	res.send(title + ', ' + description);
 });
 
-app.post('/comments/new', (req, res) => {
-  const newComment = {
-    name: req.body.name,
-    comment: req.body.comment
-  };
-
-  comments.push(newComment);
-  res.redirect('/comments');
+app.get('/topic/:id', function(req, res){
+	var topics = [
+		'Javascript is...',
+		'Nodejs is...',
+		'Express is...'
+	];
+	var output = `
+		<a href="/topic/0">Javascript</a><br>
+		<a href="/topic/1">Nodejs</a><br>
+		<a href="/topic/2">Express</a><br>
+		${topics[req.params.id]}
+	`
+	res.send(output);
 });
 
-app.get('/comments/:id', (req, res) => {
-  const id = req.params.id;
-  const comment = comments[id];
-
-  res.render('show', { title: 'Comment', comment: comment });
+app.get('/topic/:id/:mode', function(req, res){
+	res.send(req.params.id + ', ' + req.params.mode);
 });
 
-app.get('/comments/:id/edit', (req, res) => {
-  const id = req.params.id;
-  const comment = comments[id];
-
-  res.render('edit', { title: 'Edit comment', comment: comment });
+app.get('/template', function(req, res){
+	res.render('temp', {time: Date(), title: 'Jade'});
 });
 
-app.post('/comments/:id/edit', (req, res) => {
-  const id = req.params.id;
-  const comment = comments[id];
-
-  comment.name = req.body.name;
-  comment.comment = req.body.comment;
-
-  res.redirect('/comments');
+app.get('/dynamic', function(req, res){
+	var lis = '';
+	for(var i=0; i<5; i++){
+		lis = lis + '<li>coding</li>';
+	}
+	var time = Date();
+	var output = `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+		</head>
+		<body>
+			Hello, Dynamic!
+			<ul>
+			${lis}
+			</ul>
+			${time}
+		</body>
+		</html>
+	`;
+	res.send(output);
 });
 
-app.get('/comments/:id/delete', (req, res
+app.listen(3000, function(){
+	console.log('Connected, 3000 port!');
+});
+
