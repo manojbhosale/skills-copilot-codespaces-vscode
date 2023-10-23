e0ebf9b56542de0f2094da03a6ba948e5a8a39cd
Manojkumar <manojbhosale@users.noreply.github.com>
2023-10-23 07:51:43 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,2 +1,79 @@
-// create web server with comment function
-// 2016-01-22
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// Path: comment.js
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// Path: comment.js
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// Path: comment.js
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+// Path: comment.js
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+// Path: comment.js
+// create web server with express
+// express is a web framework for node.js
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+//
